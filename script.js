7771fe0077959bb80292f34b898e7d7b8779a632
Ahmed Shawky <ahmedshawky10001@gmail.com>
2026-05-25 21:09:33 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.querySelector('nav ul');
-
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
